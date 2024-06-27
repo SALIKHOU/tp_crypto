b4856a09bd6f370552ca71c056319efc73dba4b7
abdoulaye <papabdoulaye16@gmail.com>
2024-06-27 15:17:29 +0000

    first commit

--- diffie-hellman/from-libraries/test.py
+++ diffie-hellman/from-libraries/test.py
@@ -1,36 +1,40 @@
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_public_key,
+    load_pem_private_key
+)
 
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-
+# Générer les paramètres DH
 parameters = dh.generate_parameters(generator=2, key_size=1024)
-# In a real handshake the peer is a remote client. For this
-# example we'll generate another local private key though. Note that in
-# a DH handshake both peers must agree on a common set of parameters.
+
+# Générer la clé privée du serveur
 private_key_server = parameters.generate_private_key()
-# Calcul de la clé publique du client
+
+# Calculer la clé publique du serveur
 public_key = private_key_server.public_key()
 
-
-private_pem=private_key_server.private_bytes(encoding=serialization.Encoding.PEM,
-   format=serialization.PrivateFormat.PKCS8,
-   encryption_algorithm=serialization.NoEncryption()
+# Sérialiser la clé privée en PEM sans chiffrement
+private_pem = private_key_server.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
 )
 
 print("Private Key:", private_pem)
 
-privkey = load_pem_private_key(private_pem, password=None)
-print("Recupération ", isinstance(privkey, dh.DHPrivateKey))
+# Charger la clé privée à partir de PEM
+loaded_private_key = load_pem_private_key(private_pem, password=None)
+print("Récupération de la clé privée:", isinstance(loaded_private_key, dh.DHPrivateKey))
 
-
-
-
-
-public_pem=public_key.public_bytes(encoding=serialization.Encoding.PEM,
-   format=serialization.PublicFormat.SubjectPublicKeyInfo)
+# Sérialiser la clé publique en PEM
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
 
 print("Public Key:", public_pem)
 
-pubkey = load_pem_public_key(public_pem)
-print(isinstance(pubkey, dh.DHPublicKey))
+# Charger la clé publique à partir de PEM
+loaded_public_key = load_pem_public_key(public_pem)
+print("Récupération de la clé publique:", isinstance(loaded_public_key, dh.DHPublicKey))
